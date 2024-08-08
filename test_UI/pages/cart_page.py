c3deb037bba25bd8ef72eb7c2261bd3a833ca508
Fedor Zmushko <fedoszmushko@gmail.com>
2024-08-08 19:38:56 +0300

    my diploma

---
+++ test_UI/pages/cart_page.py
@@ -0,0 +1,17 @@
+from test_UI.locators.cart_locators import SELECTED_HEADPHONES
+from test_UI.pages.base_page import BasePage
+
+
+class CartPage(BasePage):
+
+
+    def check_cart_title(self):
+        page_title = self.driver.title
+        assert page_title == "Корзина заказов onliner.by", (f"Navigation wrong. {page_title=} "
+                                                            f"!= 'Корзина заказов onliner.by'")
+
+    @property
+    def get_selected_hp(self):
+        self.wait_for(SELECTED_HEADPHONES)
+        title = self.driver.find_element(*SELECTED_HEADPHONES)
+        return title.text
